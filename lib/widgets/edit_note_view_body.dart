97345d04119c6aa59ca2dcbe4c464e7298319257
essamalrawi <essamhamdy198@gmail.com>
2024-11-05 17:05:06 +0200

    take input and validate data

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
-import 'package:notes_app/widgets/custom_search_icon.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
@@ -10,28 +9,39 @@ class EditNoteViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          CustomAppBar(
-            title: "Edit note",
-            icon: Icons.check,
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          CustomTextField(hint: 'Title'),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'Content',
-            maxLines: 5,
-          ),
-        ],
-      ),
+      child: AddNoteForm(),
+    );
+  }
+}
+
+class AddNoteForm extends StatelessWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        SizedBox(
+          height: 50,
+        ),
+        CustomAppBar(
+          title: "Edit note",
+          icon: Icons.check,
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        CustomTextField(hint: 'Title'),
+        SizedBox(
+          height: 16,
+        ),
+        CustomTextField(
+          hint: 'Content',
+          maxLines: 5,
+        ),
+      ],
     );
   }
 }
